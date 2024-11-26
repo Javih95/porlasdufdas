00e4e60647342821143dde5060b608d07522ba6b
Javih95 <138259835+Javih95@users.noreply.github.com>
2024-11-25 22:06:39 -0300

    Delete models directory

--- models/autorModel.js
+++
@@ -1,7 +0,0 @@
-const db = require ("../data/db.js")
-const {DataTypes} = require ("sequelize")
-const autorModel = db.define ("autores",{
-    name : {type:DataTypes.STRING},
-    surname : {type:DataTypes.STRING},
-})
-module.exports= autorModel 
--- models/colorModel.js
+++
@@ -1,6 +0,0 @@
-const db = require ("../data/db.js")
-const {DataTypes} = require ("sequelize")
-const colorModel = db.define ("colores",{
-    nombre : {type:DataTypes.STRING},
-})
-module.exports= colorModel
