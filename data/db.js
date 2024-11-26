fb8b992a9016694d20760f1ec553b9180bb17447
Javih95 <138259835+Javih95@users.noreply.github.com>
2024-11-25 22:06:33 -0300

    Delete data directory

--- data/db.js
+++
@@ -1,9 +0,0 @@
-const {Sequelize} = require ("sequelize")
-
-const db = new Sequelize ("stockDb","root","",{
-    host : "localhost",
-    dialect:"mysql",
-    port: 3306
-})
-
-module.exports = db
